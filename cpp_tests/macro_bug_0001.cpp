e31823e48b5e395621cf02c7b311e5b4df47ee29
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2017-01-18 16:09:41 +0700

    add macro bug example

---
+++ cpp_tests/macro_bug_0001.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define test(val) \
+{ \
+    int some = 10; \
+    printf("val: %d, some: %d\n", val, some); \
+}
+
+
+int main(void) 
+{
+  test(20);
+  int x1 = 30;
+  test(x1);
+  int some = 40;
+  test(some);
+}
