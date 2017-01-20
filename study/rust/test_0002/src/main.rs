71cd4d4f3b79ad884dd4e6915e739f192c1bf11b
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2017-01-20 13:55:25 +0700

    test

--- study/rust/test_0002/src/main.rs
+++ study/rust/test_0002/src/main.rs
@@ -1,9 +1,31 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Wtf the number?");
-    println!("please type a number.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("error get a number");
-    println!("your number: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        //println!("secret number: {}", secret_number);
+        println!("please type a number.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("error get a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
+
+        println!("your number: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("too small"),
+            Ordering::Greater   => println!("too big"),
+            Ordering::Equal     => {
+                println!("Win!");
+                break;
+            }
+        }
+    }
 }
