cb4c692374bac6ffac86eac5bdfbcdd9415f0c44
Aleksey Durachenko <durachenko.aleksey@gmail.com>
2017-02-16 12:33:55 +0700

    add error messages

--- study/rust/rixit-test-2/src/img.rs
+++ study/rust/rixit-test-2/src/img.rs
@@ -6,15 +6,22 @@ use rocket::response::NamedFile;
 
 
 #[get("/img/<id>")]
-fn img(id : u32) -> Option<NamedFile> {
+fn img(id: u32) -> Option<NamedFile> {
     match env::var(::consts::RIXIT_IMG_PATH) {
         Ok(img_path) => {
             let mut filename = PathBuf::from(img_path);
             filename.push(id.to_string());
             filename.set_extension("jpg");
-            NamedFile::open(filename).ok()
+            match NamedFile::open(&filename) {
+                Ok(val) => Some(val),
+                Err(_) => {
+                    println!("file not found: {}", filename.as_path().to_string_lossy());
+                    None
+                }
+            }
         }
         Err(_) => {
+            println!("environment variable {} is not set", ::consts::RIXIT_IMG_PATH);
             None
         }
     }
